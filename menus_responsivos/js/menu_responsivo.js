b65e088a73fc04968cb12a6695232583c50a747c
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2013-09-14 15:57:59 -0400

    borrado

--- menus_responsivos/js/menu_responsivo.js
+++
@@ -1,9 +0,0 @@
-var resaltar = function (id) {
-	for (var i = 0; i < 6; i++){
-		var link=document.links[i];
-		link.style.backgroundColor=black;
-	};
-	document.getElementById(id).style.backgroundColor="white";
-	document.getElementById(id).style.color="black";
-
-}
